111f27c5b46ed5bb3a834770e2f6e5318485bc9a
David Portilla <dportillalara@gmail.com>
2026-05-13 17:52:27 -0400

    feat: migrate database from PostgreSQL to SQLite and update Product model with soft delete functionality

--- prisma/migrations/20260413172228_soft_delet/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "available" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN     "deletedAt" TIMESTAMP(3);
---
+++ prisma/migrations/20260513204800_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deletedAt" DATETIME
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
