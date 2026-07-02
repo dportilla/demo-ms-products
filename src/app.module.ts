b2f8ca21429cfd9e403892155a79caf1ce1ea688
David Portilla <dportillalara@gmail.com>
2026-07-02 00:13:37 -0400

    feat: replace configuration loading with systemConfig in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '@/src/common/prisma/prisma.module';
-import { configuration } from '@/src/config/configuration.config';
 import { envValidationSchema } from '@/src/config/env.validation';
+import { systemConfig } from '@/src/config/system.config';
 import { ProductsModule } from '@/src/products/products.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			load: configuration,
+			load: systemConfig,
 			validationSchema: envValidationSchema,
 		}),
 		ProductsModule,
--- src/config/system.config.ts
+++ src/config/system.config.ts
@@ -2,4 +2,4 @@ import appConfig from '@/src/config/app.config';
 import dbConfig from '@/src/config/db.config';
 import natsServer from '@/src/config/nats.server';
 
-export const configuration = [appConfig, dbConfig, natsServer];
+export const systemConfig = [appConfig, dbConfig, natsServer];
